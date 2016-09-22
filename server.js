81dcd707e58ac25fc9f37692ff8d345fd01d173e
khursheedali38 <khursheedali38@gmail.com>
2016-09-23 00:27:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {                         //when url '/' is sel
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req, res){
+    res.sendFile("Hello everyone!!!!!!Addressing first article");
+}) ;
+
+app.get('/article-two',function(req, res){
+    res.sendFile("Hello everyone!!!!!!Addressing second article");
+}) ;
+
+app.get('/article-three',function(req, res){
+    res.sendFile("Hello everyone!!!!!!Addressing third article");
+}) ;
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
